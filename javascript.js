5e6c1e29105dc1a982e0247ed9c05609c6c61047
murkyweb <murkyweb@protonmail.com>
2022-02-03 17:55:33 +0200

    Added UI

--- javascript.js
+++ javascript.js
@@ -1,87 +1,56 @@
+let playerScore = 0;
+let computerScore = 0;
+let winner = '';
+
 function computerPlay() {
     const choices = ['Rock', 'Paper', 'Scissors'];
     let randomChoice = choices[Math.floor(Math.random() * choices.length)];
     return randomChoice;
 }
 
-function playRound (playerSelection, computerSelection) {
-    playerSelection = prompt("Rock, Paper or Scissors?", '');
-    computerSelection = computerPlay();
-    if (playerSelection.toLowerCase() === 'rock') {
-        switch (computerSelection) {
-            case 'Rock':
-                result = 'Tie';
-                break;
-            case 'Paper':
-                result = 'You Lose!';
-                break;
-            case 'Scissors':
-                result = 'You Win!';
-        }
-        return result;
-    }
-    else if (playerSelection.toLowerCase() === 'paper') {
-        switch (computerSelection) {
-            case 'Rock':
-                result = 'You Win!';
-                break;
-            case 'Paper':
-                result = 'Tie';
-                break;
-            case 'Scissors':
-                result = 'You Lose!';
-        }
-        return result;
-    }
-    else if (playerSelection.toLowerCase() === 'scissors') {
-        switch (computerSelection) {
-            case 'Rock':
-                result = 'You Lose!';
-                break;
-            case 'Paper':
-                result = 'You Win!';
-                break;
-            case 'Scissors':
-                result = 'Tie';
-        }
-        return result;
-    }
-    return result;
+function declareWinner() {
+    return winner = (
+        playerScore === 5 ? 'Player' :
+        computerScore === 5 ? 'Computer' :
+        null
+    );
 }
 
-function game() {
-    let winner = '';
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        playRound();
-        if (result === 'You Win!') {
+function playRound (playerSelection, computerSelection) {
+    computerSelection = computerPlay();
+
+    if (playerSelection === 'rock' && computerSelection === 'Scissors' || 
+        playerSelection === 'paper' && computerSelection === 'Rock' ||
+        playerSelection === 'scissors' && computerSelection === 'Paper') {
             playerScore += 1;
-            console.log(result);
-            console.log(playerScore);
-            console.log(computerScore);
-        } 
-        else if (result === 'You Lose!') {
+            lowerSide.setAttribute('style', 'border: 2px solid green;')
+    } else if (playerSelection === 'rock' && computerSelection === 'Paper' ||
+        playerSelection === 'paper' && computerSelection === 'Scissors' ||
+        playerSelection === 'scissors' && computerSelection === 'Rock') {
             computerScore += 1;
-            console.log(result);
-            console.log(playerScore);
-            console.log(computerScore);
+            lowerSide.setAttribute('style', 'border: 2px solid red;')
         } else {
-            playerScore += 1;
-            computerScore += 1;
-            console.log(result);
-            console.log(playerScore);
-            console.log(computerScore);
+            lowerSide.setAttribute('style', 'border: 2px solid yellow;')
         }
-    }
-    if (playerScore > computerScore) {
-        winner = 'Player Wins!';
-    }
-    else if (playerScore < computerScore) {
-        winner = 'Computer Wins!';
-    } else {
-        winner = 'Tie';
-    }
-    return winner;
 }
+
+const buttons = document.querySelectorAll('button');
+const result = document.querySelector('.roundResult');
+const finalResult = document.querySelector('.finalResult');
+const lowerSide = document.querySelector('.lower-side');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound(button.id);
+        result.textContent = `The score is ${playerScore} : ${computerScore}.`;
+        declareWinner();
+        if (winner) {
+            finalResult.textContent = `${winner} wins!`;
+            playerScore = 0;
+            computerScore = 0;
+        } else {
+           finalResult.textContent = ''; 
+        }
+    });
+});
 
